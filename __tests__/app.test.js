8d37da94e45f4436a6fe6612b5697e26932503a7
EmilyDSarani <emily.sarani@gmail.com>
2021-11-05 12:30:28 -0500

    RED FAILING GET- I think I misunderstood the assignment, but I have already made it this far, so there is no turning back

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
-const app = require('../lib/app');
+const app = require('../app');
 
 describe('app routes', () => {
-  it('should get back the /posty route with POST', async() => {
-    const response = await request.post('/posty').send('this is a posty post');
-    expect(response).toEqual('this is a posty post');
+  it('should get back the /getit route with Get', async() => {
+    const response = await request (app).get('/getit');
+    expect(response.body).toEqual('Sup Friend?');
   });
 });
